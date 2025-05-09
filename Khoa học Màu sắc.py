3fe69e5153bef75b16308895f4eca3776c87206f
DoanLeVietPhat <22158077@student.hcmute.edu.vn>
2025-05-09 14:27:48 +0700

    Add files via upload

---
+++ Khoa học Màu sắc.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Khoa học Màu sắc", layout="wide"
+)
+
+st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của mình!")
+st.write("# Mình tên là ĐOÀN LÊ VIỆT PHÁT,  MaSV 22158077")
+st.write("# Mình tên là NGUYỄN HỒNG ĐỨC,    MaSV 22158053")
+st.write("# Mình tên là VŨ MẠNH TUẤN,       MaSV 22158106")
+
+
